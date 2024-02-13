f782806924853916143b73b64f698c81b30275d8
Desktop Marllon <marllonrsantos@gmail.com>
2024-02-13 16:20:24 -0300

    Feat: Getting a user by id

--- src/main/java/com/marllonrodrigues/workshopmongo/services/UserService.java
+++ src/main/java/com/marllonrodrigues/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.marllonrodrigues.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marllonrodrigues.workshopmongo.domain.User;
 import com.marllonrodrigues.workshopmongo.repository.UserRepository;
+import com.marllonrodrigues.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
