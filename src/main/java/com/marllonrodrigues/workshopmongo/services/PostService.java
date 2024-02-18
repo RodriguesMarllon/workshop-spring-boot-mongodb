042f6b1fdade8089fb943cae4760030dd9a7d100
Desktop Marllon <marllonrsantos@gmail.com>
2024-02-18 20:44:34 -0300

    Feat: Getting a post by id

---
+++ src/main/java/com/marllonrodrigues/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.marllonrodrigues.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marllonrodrigues.workshopmongo.domain.Post;
+import com.marllonrodrigues.workshopmongo.repository.PostRepository;
+import com.marllonrodrigues.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
